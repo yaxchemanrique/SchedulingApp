bd9159c0ddc2095151d5643efefd5fb39357e1ea
YaxcheM <yaxchemanrique@gmail.com>
2022-06-20 13:37:59 -0700

    feat: Open Time Modal and Open Success Modal

---
+++ assets/index.js
@@ -0,0 +1,25 @@
+const timeDialog = document.querySelector('#time-dialog');
+const currentMonthDaysNodeList = document.querySelectorAll('.current-month-day');
+const currentMonthDays = [...currentMonthDaysNodeList];
+
+const successDialog = document.getElementById('sucess-dialog');
+const buttonAnimation = document.getElementById('checkmark-svg');
+const submitBtn = document.getElementById('submit-btn');
+
+// Adds event listener to all days in the month to open the Time modal to select time to book
+currentMonthDays.forEach(currentMonthDay => openModal('click', currentMonthDay, timeDialog))
+
+//Function that sets an event Listener and opens its modal
+function openModal(listener, target, modal) {
+    target.addEventListener(listener, ()=> {
+        modal.showModal();
+    })
+}
+
+submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    submitBtn.style.pointerEvents = 'none';
+    submitBtn.classList.add('animating');
+});
+
+openModal('animationend', buttonAnimation, successDialog);
