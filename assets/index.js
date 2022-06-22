ff00e030875adbada99b32aa792d7a57ec278eab
YaxcheM <yaxchemanrique@gmail.com>
2022-06-21 17:18:40 -0700

    feat: update class in nav bar steps, update class of selected appt type - This will add a modifyer to the step to make clear to the user what     stap are they in the form. - It will show which is the appointment type  selected by the user by     adding a green rectangle around it

--- assets/index.js
+++ assets/index.js
@@ -10,7 +10,27 @@ const multistepForm = document.getElementById('multistep-form');
 const formSteps = [...multistepForm.querySelectorAll('[data-step]')];
 
 const nextButton = [...multistepForm.querySelectorAll('[data-next-btn]')];
-const navBarStep = [...document.querySelectorAll('.nav-bar')];
+const navBarSteps = [...document.querySelectorAll('.navbar-step')];
+
+const appointmentOptionsContainer = [...document.querySelectorAll('input[name="appointment-type"]')];//document.querySelector('.grid-container');
+const appointmentOptionsArray = [...document.querySelectorAll('.form-group-radio')];
+
+let selctedAppointmentType;
+
+appointmentOptionsContainer.forEach(input => {
+    input.addEventListener('click', (e)=> {
+        appointmentOptionsArray.forEach(type => {
+            let selectedRadio = [...document.querySelectorAll('input[type="radio"] + label')];
+            selectedRadio.forEach(radio => {
+                radio.removeAttribute('checked');
+                radio.parentElement.classList.remove('checked-radio');
+            });
+        });
+        e.target.setAttribute('checked', true)
+        e.target.parentElement.classList.add('checked-radio');
+        selctedAppointmentType = e.target.value;
+    })
+})
 
 // Adds event listener to all days in the month to open the Time modal to select time to book
 currentMonthDays.forEach(currentMonthDay => openModal('click', currentMonthDay, timeDialog))
@@ -36,19 +56,26 @@ let currentStep = formSteps.findIndex(step => {
 if (currentStep < 0) {
     currentStep = 0;
     formSteps[currentStep].classList.add('active');
-    showCurrentStep();
+    cardCurrentStep();
+    navbarCurrentStep()
 }
 
 nextButton.forEach(button => {
     button.addEventListener('click', ()=> {
         currentStep += 1;
-        console.log(currentStep);
-        showCurrentStep();
-    })
+        cardCurrentStep();
+        navbarCurrentStep()
+    });
 })
 
-function showCurrentStep() {
+function cardCurrentStep() {
     formSteps.forEach((step, index) => {
-        step.classList.toggle('active', index === currentStep)
-    })
+        step.classList.toggle('active', index === currentStep);
+    });
+}
+
+function navbarCurrentStep() {
+    navBarSteps.forEach((navbarItem, index)=> {
+        navbarItem.classList.toggle('current-step', index === currentStep);
+    });
 }
