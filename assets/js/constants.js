65cc8e2491de0a4a5928309a06e8173cfa2cba51
YaxcheManrique <yaxchemanrique@gmail.com>
2022-09-05 09:46:21 -0700

    Feat: Rendering time tags using SCHEDULES aray and consulting availability fom DB

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,5 +1,8 @@
 const API_URL_APPOINTMENTS = 'http://localhost:3000/appointmentSlots';
 
+const SCHEDULES = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+
 export {
     API_URL_APPOINTMENTS,
+    SCHEDULES,
 };
